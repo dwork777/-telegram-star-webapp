d3bab670af77f8751874dffdd6feb2cb7bd487ec
dwork777 <removem321@proton.me>
2025-05-20 11:15:47 +0900

    Add files via upload

---
+++ telegram-star-webapp/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Telegram Star WebApp</h1>
+      <p>This is your Next.js WebApp homepage!</p>
+    </div>
+  );
+}
